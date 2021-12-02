69e43698c6686dea81bebb856f30c3ec3363bfbd
Vikas Kumar Azad <surajnimeshh1000@gmail,com>
2021-12-02 21:41:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './Header'
+import Banner from './Banner'
+import Card from './Card'
+import Home from './Home'
+import Search from './Search.js'
+import SearchResults from './SearchResults'
+import SearchPage from './SearchPage'
+import Footer from './Footer'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Header />
+        
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        
+        <Footer />
+      </ Router>
+    </>
+  )
 }
 
-export default App;
+export default App
